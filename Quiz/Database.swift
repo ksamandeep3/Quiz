9670f30e26134494cc37803497f5eb0a1dd4149f
ksamandeep3 <56143618+ksamandeep3@users.noreply.github.com>
2019-11-18 17:39:25 -0500

    Add files via upload

---
+++ Quiz/Database.swift
@@ -0,0 +1,80 @@
+//
+//  File.swift
+//  Quiz
+//
+//  Created by MacStudent on 2019-11-12.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Database{
+    
+    static var quiz1 = "What is the colour of sun?"
+   static  var ans1Num = 2
+   // Accelerometer
+     static   var optionsQ1 = ["Blue","Green","Yellow","Black"]
+    
+    
+  static   var quiz2 = "What is the colour of grass?"
+   static  var ans2Num = 1
+
+  //  Document Object Model
+  
+   static   var optionsQ2 = ["White","Green","Grey","Yellow"]
+    
+  static   var quiz3 = "What is the colour of milk?"
+   static  var ans3Num = 0
+//    1024X768
+
+   static   var optionsQ3 = ["White","Pink","Black","Orange"]
+    
+ static    var quiz4 = "What is the colour of an Orange?"
+  static   var ans4Num = 2
+ 
+  //  Yes
+   
+   static   var optionsQ4 = ["Pale Yellow","Brown","Orange","none  of them"]
+    
+  static   var quiz5 = "What is the colour of sky?"
+  static   var ans5Num = 0
+  //  Simple API for XML
+   
+   static   var optionsQ5 = ["Blue","Silver","Golden","White"]
+    
+   static  var quiz6 = "What is the colour of kiwi?"
+  static   var ans6Num = 1
+  
+   // UI Automation API
+    
+   static   var optionsQ6 = ["Yellow","Brown","Orange","Black"]
+    
+  static   var quiz7 = "Which is the colour of blood? "
+   static  var ans7Num = 0
+   // iOS 4 and above
+
+   static   var optionsQ7 = ["Red","None","Clear colour","White"]
+    
+  static   var quiz8 = "What is the colour of clouds?"
+   static  var ans8Num = 3
+    
+   // Key-Value Observing
+  static    var optionsQ8 = ["Black","Blue","Pink","White"]
+    
+  static   var quiz9 = "What is the colour of Lemonade?"
+  static   var ans9Num = 1
+   
+   // With Instruments
+   
+  static    var optionsQ9 = ["Golden","Yellow","Purple", "Black"]
+    
+  static   var quiz10 = "What is the colour of plum?"
+  static   var ans10Num = 3
+   
+   // By using dequeReusableCellWithIdentifier
+ 
+ //Data model objects
+ 
+ 
+    static  var optionsQ10 = ["Grey","Purple","Red","Orange"]
+}
